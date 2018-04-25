9f4ec949124f48abef73f50f4e814cf80ca9d162
lyrieek <tianhao7@foxmail.com>
2018-04-25 23:57:52 +0800

    add template

--- src/Pers/th/Scanner.scala
+++ src/Pers/th/Scanner.scala
@@ -7,10 +7,14 @@ object Scanner {
   def main(args: Array[String]): Unit = {
     val file = Source.fromFile("resource/r1.template")
     for (line <- file.getLines) {
-      println(line)
+      println(line match {
+        case "<<black>>" =>
+          println("template"+line)
+        case _ => line
+      })
+      line.matches("<<[a-zA-Z]+>>")
     }
     file.close
-
   }
 
 }
