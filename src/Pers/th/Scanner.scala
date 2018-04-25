0538cf5ca70f52042808334b8737fe8ee5b5718f
lyrieek <tianhao7@foxmail.com>
2018-04-26 00:13:10 +0800

    improve scanner

--- src/Pers/th/Scanner.scala
+++ src/Pers/th/Scanner.scala
@@ -1,18 +1,23 @@
 package Pers.th
 
+import java.util.regex.Pattern
+
 import scala.io.Source
 
 object Scanner {
 
   def main(args: Array[String]): Unit = {
-    val file = Source.fromFile("resource/r1.template")
+    val file = Source fromFile "resource/r1.template"
+    val regex = Pattern compile "<<[a-zA-Z]+>>"
     for (line <- file.getLines) {
-      println(line match {
-        case "<<black>>" =>
-          println("template"+line)
-        case _ => line
-      })
-      line.matches("<<[a-zA-Z]+>>")
+//      line match {
+//        case "<<black>>" =>
+//        case _ =>
+//      }
+      val item = regex matcher line
+      while (item.find){
+        println(item group)
+      }
     }
     file.close
   }
