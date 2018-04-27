5f2a59766de9fa04cdaf19acb7dadc68622c9912
lyrieek <tianhao7@foxmail.com>
2018-04-27 17:54:32 +0800

    improve scanner

--- src/Pers/th/Scanner.scala
+++ src/Pers/th/Scanner.scala
@@ -9,34 +9,34 @@ import scala.util.matching.Regex
 
 object Scanner {
 
-//  val regex: Pattern = Pattern compile "<<[a-zA-Z]+(\\d+)?>>"
+  //  val regex: Pattern = Pattern compile "<<[a-zA-Z]+(\\d+)?>>"
   val regex: Regex = "\\$\\{[a-zA-Z]+(\\d+)?}".r
-  var content: String = ""
   var variable: Set[Expression] = Set()
 
-  def main(args: Array[String]): Unit = {
-    val files:Array[File] = new File("resource\\vs-module").listFiles()
-    read(files(0).getAbsolutePath)
-//    read("resource/r1.template")
-    for (elem <- variable) {
-      println(elem.value)
-    }
-  }
+  val files: Array[File] = new File("resource\\vs-module").listFiles()
 
+  def main(args: Array[String]): Unit = {
+    for (file <- files) {
+      read(file.getAbsolutePath)
+    }
+    //    read("resource/r1.template")
+    for (elem <- variable)
+      println(elem.value)
+  }
 
   def read(path: String): Unit = {
     val file = Source fromFile path
-    for (line <- file.getLines) {
-      regex findFirstIn line foreach(item => {
-        content += item
-        variable += new VariableExpr(item)
-      })
-    }
+    for (line <- file.getLines)
+      regex findAllIn line foreach
+        (item => variable += new VariableExpr(item))
     file.close
   }
-  
-  def exec(fn: Function[Unit,Unit]): Unit = {
-    fn.apply()
-  }
+
+  //  exec(_ => {
+  //    println(1)
+  //  })
+  //  def exec(fn: Function[Unit,Unit]): Unit = {
+  //    fn.apply()
+  //  }
 
 }
